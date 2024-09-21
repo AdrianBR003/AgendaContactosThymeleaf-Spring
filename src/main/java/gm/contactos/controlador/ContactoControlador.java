f1565bf19d0cbc6e34ddaf99bf6ed1bc9f9ce497
Adrián Benítez Roldán <149155965+AdrianBR003@users.noreply.github.com>
2024-09-21 14:51:23 +0200

    add: EliminarContacto
    + update: index.html
    + update: ContactoControlador.java

--- src/main/java/gm/contactos/controlador/ContactoControlador.java
+++ src/main/java/gm/contactos/controlador/ContactoControlador.java
@@ -6,10 +6,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -40,5 +39,28 @@ public class ContactoControlador {
         logger.info("Contacto a agregar: " + contacto);
         contactoServicio.guardarContacto(contacto);
         return "redirect:/";
+    }
+
+    @GetMapping("/editar/{id}")
+    public String mostrarEditar(@PathVariable(value="id") int idContacto, ModelMap modelo){
+        Contacto contacto = contactoServicio.buscarContactoPorId(idContacto);
+        logger.info("Contacto a Editar (mostrar): " + contacto );
+        modelo.put("contacto", contacto);
+        return "editar"; // editar.html
+    }
+
+    @PostMapping("/editar")
+    public String editar(@ModelAttribute("contacto") Contacto contacto){
+        logger.info("Contacto a guardar (editar) : " + contacto);
+        contactoServicio.guardarContacto(contacto);
+        return "redirect:/";
+    }
+
+    @GetMapping("/eliminar/{id}")
+    public String eliminar(@PathVariable(value ="id") int idContacto){
+        Contacto contacto = contactoServicio.buscarContactoPorId(idContacto);
+        logger.info("Contacto a eliminar: " + contacto);
+        contactoServicio.eliminarContacto(contacto);
+        return "redirect:/"; // Redirigir a la página de contactos
     }
 }
